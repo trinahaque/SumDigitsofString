8abc4a15647c9da067fb51997ac6132540f089eb
trina <trina_haque28@outlook.com>
2018-10-05 11:37:01 -0700

    Refactor code and add more comments

--- sumDigitString.py
+++ sumDigitString.py
@@ -1,16 +1,25 @@
-# this function takes a string, a file or a hex value as an input. 
-# It returns the sum of the digits in the input.
+# This function takes a string, a file or a hex value as an input
+# "-f" in input1 specifies that the input2 will contain a file with string in it
+# "-x" in input1 specifies that the input2 will contain a hex string
+# Only 1 input specifies that the input is just a string. input2's default value will be none
+# It returns the sum of the digits in the string
+# It returns None if an invalid input is entered
 def sumDigitString(input1, input2=None):
+    # checks if the input is a file
     if input1 == "-f" and input2 != None:
         file = open(input2, "r")
         stringFromFile = file.read()
         result = findSum(stringFromFile)
         file.close()
+    # checks if the input is a hexstring
     elif input1 == "-x" and input2 != None:
+        # hexInt converts the input hex string to an int
         hexInt = int(input2, 16)
         result = findSum(str(hexInt))
+    # checks if the input is a string
     elif isinstance(input1, str):
         result = findSum(input1)
+    # checks for all invalid input. Returns None in that case
     else:
         return None
     return result
@@ -25,18 +34,13 @@ def findSum(string):
 
 # the main calls various function with different test cases
 if __name__ == "__main__":
-    filename = "files.txt"
-    output = sumDigitString("-f", filename)
-    print (output)
-
-    output = sumDigitString("-x", "abc123")
-    print (output)
-
-    output = sumDigitString("abc123")
-    print (output)
-
-    output = sumDigitString("abc")
-    print (output)
-
-    output = sumDigitString(123)
-    print (output)
+    # running the function with two inputs.  "-f" in input 1 it's a file input and "files.txt" in input2 has a file with string in it
+    print (sumDigitString("-f", "files.txt"))
+    # running the function with two inputs. "-x" in input 1 specifies it's a hex string, input2 is a hex string
+    print (sumDigitString("-x", "abc123"))
+    # running the function with one input that is string with digits in it
+    print (sumDigitString("abc123"))
+    # running the function with one input that is string without digits in it
+    print (sumDigitString("abc"))
+    # running the function with one with invalid input
+    print (sumDigitString(123))
--- unitTest.py
+++ unitTest.py
@@ -6,19 +6,19 @@ class TestSumDigitStrings(unittest.TestCase):
     def test_nonumber(self):
         self.assertEqual(sumDigitString("abc"), 0)
     
-    # tests a string with number in it
+    # tests a string with number 123 in it and checks if it equals to 6
     def test_withnumber(self):
         self.assertEqual(sumDigitString("abc123"), 6)
 
-    # tests a hex number in it
+    # tests a hex number in abc123 it. Checks if number in it adds up to 33
     def test_hexnumber(self):
         self.assertEqual(sumDigitString("-x", "abc123"), 33)
 
-    # tests a file input
+    # tests a file input that has "He2ll4o" in it. Check if it equals to 6
     def test_file(self):
         self.assertEqual(sumDigitString("-f", "files.txt"), 6)
 
-    # tests a invalid input
+    # tests a invalid input. Check if it equals to None
     def test_invalid(self):
         self.assertEqual(sumDigitString(123), None)
 
